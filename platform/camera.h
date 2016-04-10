f9ec3fb20aba8711ee375d386204877dec5a7fab
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2016-04-10 12:06:10 -0400

    Define Sensitivity Values

--- platform/camera.h
+++ platform/camera.h
@@ -8,6 +8,13 @@
 
 #define CAMERA_SENSITIVITY 2
 
+#define SENSITIVITY_LEE 0
+#define SENSITIVITY_1 1
+#define SENSITIVITY_2 2
+#define SENSITIVITY_3 3
+#define SENSITIVITY_4 4
+#define SENSITIVITY_5 5
+
 // Initializes the infrared camera
 bool init_camera(TWI_t* port);
 
